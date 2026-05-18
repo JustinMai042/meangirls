a8cb8986ca7910876f2ba3b48396ab924459d34e
Justin Mai <jmai25@calpoly.edu>
2026-05-18 15:20:24 -0700

    push

--- R/give_candygrams.R
+++ R/give_candygrams.R
@@ -6,7 +6,7 @@
 #'
 #' @return A candy gram announcement
 #'
-#' @importFrom stringr str_detect str_to_title
+#' @importFrom stringr str_detect str_to_title str_trim
 #' @importFrom english as.english
 #'
 #' @export
@@ -30,7 +30,8 @@ give_candygrams <- function(person, number,
   number <- str_to_title(as.english(number))
 
 
-  glue::glue("{number} for {person}.")
+  glue::glue("{number} for {person}. {extra_message}") %>%
+    str_trim(side = "both")
 
 
 
